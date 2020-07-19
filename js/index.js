262719503335eaa19f322da201e5cf922e3a9ca8
djon15st93 <djon15st93@gmail.com>
2020-07-19 14:47:09 +0000

    swiper

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ let parallaxInstance = new Parallax(scene, {
   relativeInput: true
 });
 parallaxInstance.friction(0.2, 0.2);
+let swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween:1300,
+  slidesPerGroup: 1,
+  
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+ 
+});
